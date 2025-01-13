ef8d4c2c5487c92f317e3f8d237fe895ed9e91d3
pauldhz <paul.denhez.ds@gmail.com>
2025-01-13 22:33:33 +0100

    document the explorer

--- src/main/java/org/denhez/pdf/tool/explorer/Explorer.java
+++ src/main/java/org/denhez/pdf/tool/explorer/Explorer.java
@@ -8,10 +8,65 @@ import java.util.function.UnaryOperator;
 
 public interface Explorer<U> {
 
-    Pair<Deque<String>,U> explore(Deque<String> t, Predicate<U> delimiter);
-    Pair<Deque<String>,U> explore(Deque<String> t, Predicate<U> delimiter, int chunk, String chunkSeparator);
-    Pair<Deque<String>,U> explore(Deque<String> t, int leap);
-    Pair<Deque<String>,U> explore(Deque<String> t, int leap, UnaryOperator<U> uo);
-    Pair<Deque<String>,U> explore(Deque<String> t, int leap, int chunk, String chunkSeparator);
+    /**
+     * Explores a text in a deque. The deque is made of parts of a full text (for example, each row of a text)
+     *
+     * @param text the text in a deque
+     * @param delimiter delimiter to stop exploration when predicate is true
+     * @return a pair of the remaining text (first) followed by the text found
+     */
+    Pair<Deque<String>,U> explore(Deque<String> text, Predicate<U> delimiter);
+
+    /**
+     * Variant of:
+     * @see #explore(Deque, Predicate)
+     * Explore elements by chunks and return a pair of the result using a separator
+     *
+     * @param text the text in a deque
+     * @param delimiter delimiter to stop exploration when predicate is true
+     * @param chunk chunk size
+     * @param chunkSeparator chunk separator
+     * @return a pair of the remaining text (first) followed by the text found (second) in chunk, separated by the
+     * chunkSeparator
+     */
+    Pair<Deque<String>,U> explore(Deque<String> text, Predicate<U> delimiter, int chunk, String chunkSeparator);
+
+    /**
+     * Variant of:
+     * @see #explore(Deque, Predicate)
+     * Just applying a leap as delimiter to stop exploration 
+     *
+     * @param text the text in a deque
+     * @param leap the leap to apply 
+     * @return a pair of the remaining text (first) followed by the text found (second) after the leap is made.
+     */
+    Pair<Deque<String>,U> explore(Deque<String> text, int leap);
+
+    /**
+     * Variant of
+     * @see #explore(Deque, int)
+     * with unary operator to alter then second element of the pair
+     *
+     * @param text the text in a deque
+     * @param leap the leap to apply
+     * @param uo the unary operator
+     * @return @return a pair of the remaining text (first) followed by the text found (second, transformed by the
+     * unary operator) after the leap is made.
+     */
+    Pair<Deque<String>,U> explore(Deque<String> text, int leap, UnaryOperator<U> uo);
+
+    /**
+     * Variant of
+     * @see #explore(Deque, Predicate, int, String)
+     * but with a leap instead of Predicate
+     *
+     * @param text the text in a deque
+     * @param leap the leap to apply
+     * @param chunk chunk size
+     * @param chunkSeparator chunk separator
+     * @return a pair of the remaining text (first) followed by the text found (second) after the leap is made,
+     * in chunk, separated by the chunkSeparator
+     */
+    Pair<Deque<String>,U> explore(Deque<String> text, int leap, int chunk, String chunkSeparator);
 
 }
